9c095d1d9c4cf6b919a8558e1a28dc3b9124a508
Agus Prasetyo <agusprasetyo1889@gmail.com>
2020-08-06 23:00:18 +0700

    delete connect.php

--- class/connect.php
+++ class/connect.php
@@ -13,7 +13,7 @@
       {
          $hostname = "localhost";
          $username = "root";
-         $password = "gokpras123";
+         $password = "";
          $database = "toko";
 
          $this->koneksi = mysqli_connect($hostname, $username, $password, $database) or trigger_error(mysqli_error($this->koneksi), E_USER_NOTICE);
